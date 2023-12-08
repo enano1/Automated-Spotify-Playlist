1c091f1d8d39eb70f9a78d4600f66df6fe12c3f4
Joseph Mitchell <jmit@bu.edu>
2023-12-08 12:26:59 -0500

    added some fading effects

--- static/script.js
+++ static/script.js
@@ -1,7 +1,41 @@
+// Handles the loading animation
 var pressed = false;
 var loader = document.getElementById('loader');
-
-// Handles the loading animation
 document.getElementById('submit-button').addEventListener('click', function(event) {
-    loader.style.display = "block";
+    // loader.style.display = "block";
+    fadeIn(loader);
 });
+
+// Function to fade in elements
+function fadeIn(element) {
+    let op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    let timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+// Function to fade out elements
+function fadeOut(element) {
+    let op = 1;  // initial opacity
+    let timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
+}
+
+window.onload = function() {
+    let form = document.getElementById('fade');
+    fadeIn(form);
+};
+
